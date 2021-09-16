febc8831bb800e32367b3362f0630430d7c61875
ser4 <pristup2040@gmail.com>
2021-09-16 14:18:09 +1000

    add form html

--- my_site_1/news/forms.py
+++ my_site_1/news/forms.py
@@ -5,11 +5,17 @@ from .models import Category
 class NewsForm(forms.Form):
     title = forms.CharField(max_length=150, label='Заголовок новости:',
                             widget=forms.TextInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(label='Контент:', required=False)
+    content = forms.CharField(label='Контент:', required=False,
+                              widget=forms.Textarea(attrs={
+                                  'class': 'form-control',
+                                  'rows': 5
+                              }))
     is_published = forms.BooleanField(label='Опубликовано:', initial=True)
+
     category = forms.ModelChoiceField(queryset=Category.objects.all(),
                                       label='Категория:',
-                                      empty_label='Выберите категорию')
+                                      empty_label='Выберите категорию',
+                                      widget=forms.Select(attrs={'class': 'form-control'}))
 
 
 
