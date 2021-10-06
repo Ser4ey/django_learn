080654d0535fa3f5b3d1a8f86c9c8d5533e547af
ser4 <pristup2040@gmail.com>
2021-10-06 21:31:38 +1000

    lesson 45 6 oct 21:31

--- my_site_1/news/views.py
+++ my_site_1/news/views.py
@@ -3,6 +3,26 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import News, Category
 from .forms import NewsForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Вы успешно зарегистрировались!')
+            form.save()
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка вилидации!')
+    else:
+        form = UserCreationForm()
+    return render(request, 'news/register.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'news/login.html', {'news': 'news'})
 
 
 class HomeNews(ListView):
